c60548049f063278bb598016fc9092744bfa57a3
Leonardnt01 <leonardo.matias.01@hotmail.com>
2026-05-21 21:39:22 -0500

    feat:feat

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -8,7 +8,8 @@ export async function createClient() {
 
   return createServerClient(
     env.NEXT_PUBLIC_SUPABASE_URL,
-    env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY,
+    env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY,  
+    //dasdsdasdasda   
     {
       cookies: {
         getAll() {
