fb0a0e087f4aaca2997ab9c6be1c740835de5ab2
LlerenaPiero <pieromanuelperu171@gmail.com>
2026-06-14 08:31:58 -0500

    fix(ci): corregir variables de entorno de ejecucion y optimizar timeouts para la nube

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,9 +8,17 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
   
+  /* AJUSTE DE TIMEOUTS PARA LA NUBE */
+  timeout: process.env.CI ? 45000 : 30000, // Le damos 45 segundos a cada test completo en CI
+  expect: {
+    timeout: process.env.CI ? 10000 : 5000, // Le damos 10 segundos a cada expect visual en CI
+  },
+
   use: {
-    baseURL: 'http://127.0.0.1:3000', 
+    baseURL: 'http://127.0.0.1:3000',
     trace: 'on-first-retry',
+    actionTimeout: process.env.CI ? 15000 : 0, // Tolerancia para escribir y dar clics en la nube
+    navigationTimeout: process.env.CI ? 20000 : 0, // Tolerancia para las redirecciones de Supabase
   },
 
   projects: [
